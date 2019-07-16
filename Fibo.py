78d1a44f2b9936d3bad9181bb467c0c7adb22857
RahulBaid99 <51899171+RahulBaid99@users.noreply.github.com>
2019-07-16 15:07:15 +0530

    Delete Fibo.py

--- Fibo.py
+++
@@ -1,9 +0,0 @@
-def fibo(n):
-    if n == 1 or n == 0:
-        return 1
-    else:
-
-            n= n-1 + n-2
-            return n;
-x=int(input('Enter the nth number of fibo series'))
-print(fibo(x))
