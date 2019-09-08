9c15128ecbba1c58b2b78b82e1c016931c246259
RahulBaid99 <51899171+RahulBaid99@users.noreply.github.com>
2019-09-08 17:32:53 +0530

    Create Hellohibye(divisible).py

---
+++ Hellohibye(divisible).py
@@ -0,0 +1,11 @@
+
+for i in range(20,61):
+    if(i%3==0 and i%5 ==0 ):
+        print("bye")
+    elif(i%3==0):
+        print("hi")
+    elif(i%5==0):
+        print("hello")
+    
+    else:
+        print(i)
